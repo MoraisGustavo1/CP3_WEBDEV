341e0352afe1ff8f3e072889475707a58f56e8df
MoraisGustavo1 <gmoraisildefonso@gmail.com>
2024-05-19 17:08:35 -0300

    Criado função de quiz - Leonardo Novaes

--- js/script.js
+++ js/script.js
@@ -33,3 +33,37 @@ function validateLogin() {
         return false;
     }
 }
+function startQuiz() {
+    const questions = [
+        "1.Qual é o mamífero mais rápido do mundo?",
+        "2.Qual animal é conhecido por sua capacidade de mudar de cor para se camuflar?",
+        "3.Qual é o maior animal marinho?",
+        "4.Qual ave é conhecida por sua habilidade de imitar sons e fala humana?",
+        "5.Quantas patas tem uma aranha?",
+        "6.Qual é o animal terrestre mais alto do mundo?",
+        "7.Qual animal é conhecido por construir represas e alterar o fluxo de rios?",
+        "8.Quantas patas tem uma formiga?",
+        "9.Qual é o animal mais lento do mundo?",
+        "10.Onça é um animal brasileiro?"
+    ];
+    const correct = [
+        "GUEPARDO",
+        "CAMALEAO",
+        "BALEIA AZUL",
+        "PAPAGAIO",
+        "8",
+        "GIRAFA",
+        "CASTOR",
+        "6",
+        "BICHO PREGUIÇA",
+        "SIM"
+    ]
+    let score = 0;
+    for (let i = 0; i < questions.length; i++) {
+        const answer = prompt(questions[i]);
+        if (answer.toUpperCase() === correct[i]) {
+            score++;
+        }
+    }
+    document.getElementById("quizResult").innerText = `Você acertou ${score} de ${questions.length} perguntas.`;
+}
