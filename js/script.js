7ca4261b1872548292f55a5b5b88327fb103e0c5
MoraisGustavo1 <gmoraisildefonso@gmail.com>
2024-05-19 17:04:42 -0300

    Criado alerta de boas vindas e slideshow - Gustavo Morais

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+function welcomeAlert() {
+    alert("Seja bem-vindo");
+}
 
+let currentSlide = 0;
+
+function showSlide() {
+    const slides = document.querySelectorAll("#slideshow img");
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.display = "block";
+}
